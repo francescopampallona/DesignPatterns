04d7a83b12a4058f5cc82aa9478548e60a92f73a
Francesco Pampallona <pampallonafrancesco@gmail.com>
2020-05-31 19:25:42 +0200

    State

---
+++ Comportamentali/State/README.md
@@ -0,0 +1,4 @@
+**SCOPO DI STATE**
+
+Permette ad un oggetto (aContext) di cambiare il suo comportamente al cambiare del suo stato interno. L'oggetto si comporterà come se avesse cambiato la sua classe.
+
---
+++ Comportamentali/State/state/src/main/java/com/francescopampallona/state/ConcreteStateA.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.francescopampallona.state;
+
+/**
+ *
+ * @author franc
+ */
+public class ConcreteStateA implements State {
+    private final String state = "ConcreteStateA";
+    @Override
+    public void showState(){
+        System.out.println(this.state);
+    }
+}
